a3aec17c6237d18416d725cf942024c2d06a0238
Carmen Macleroy Obied, PhD <carmen.obied@gmail.com>
2020-08-24 22:56:54 -0700

    updated server, refactored code, tested get, post, pull and delete endpoints in postman and re-checked database, all set

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+// Dependencies
+// =============================================================
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
@@ -5,19 +7,43 @@ const cors = require('cors')
 const db = require('./server/db')
 const portfolioRouter = require('./server/routes/portfolio-router')
 
+// Sets up the Express App
+// =============================================================
 const app = express()
-const port = 3000
+const PORT = 3000
 
+// set up the Express app to handle data parsing
+// =============================================================
+// define middleware here
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
 app.use(bodyParser.json())
 
-db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+  }
+
+// API routes
+// =============================================================
+app.use('/', portfolioRouter)
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Welcome to my Portfolio Website!')
 })
 
-app.use('/api', portfolioRouter)
+app.get('*', (req, res) => {
+    const rootHtmlPath = path.resolve(__dirname, '..', 'public', 'index.html');
+    res.sendFile(rootHtmlPath);
+  })
 
-app.listen(port, () => console.log(`Server running on port ${port}`))
+// Connect to the Mongo DB
+// =============================================================
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
+// Start the API server
+// =============================================================
+app.listen(PORT, () => {
+    console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+  });
--- server/routes/portfolio-router.js
+++ server/routes/portfolio-router.js
@@ -1,7 +1,5 @@
 const express = require('express')
-
 const ProjectController = require('../controllers/portfolio-controller')
-
 const router = express.Router()
 
 router.post('/project', ProjectController.createProject)
@@ -9,5 +7,10 @@ router.put('/project/:id', ProjectController.updateProject)
 router.delete('/project/:id', ProjectController.deleteProject)
 router.get('/project/:id', ProjectController.getProjectById)
 router.get('/projects', ProjectController.getProject)
+
+// If no API routes are hit, send the React app
+router.use( (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  });
 
 module.exports = router
